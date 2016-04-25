ee0ea5b2f16061ed758f4601db501272a704856d
erincullen1122 <anniehall16@hotmail.com>
2016-04-24 20:24:41 -0700

    Added files via upload

---
+++ text.js
@@ -0,0 +1,17 @@
+
+    $(function() {
+      $('#slides').slidesjs({
+          width: 700,
+          height: 393,
+          play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
+
+
+
+
